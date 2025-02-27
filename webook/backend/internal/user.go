8f42220b63c71270ccdea68393bc3caabc561812
gzm <gzm@1token.trade>
2025-02-27 09:31:57 +0800

    feat(backend) : impl signup reg and fix cors with middleware

--- webook/backend/internal/user.go
+++ webook/backend/internal/user.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/dlclark/regexp2"
 	"github.com/gin-gonic/gin"
-
-	"GolangLearning/webook/backend/pkg"
 )
 
 type UserHandler struct {
+	EmailRegexp *regexp2.Regexp
 }
 
-var UserRoute UserHandler
+func NewUserHandler() *UserHandler {
+	const EmailRegexpStr = "^\\w+(-+.\\w+)*@\\w+(-.\\w+)*.\\w+(-.\\w+)*$"
+	return &UserHandler{EmailRegexp: regexp2.MustCompile(EmailRegexpStr, regexp2.None)}
+}
+
+var UserRoute = NewUserHandler()
 
 func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
 	up := server.Group("/users")
@@ -36,7 +41,20 @@ func (u *UserHandler) SignUpHandler(gtx *gin.Context) {
 	if err := gtx.Bind(&req); err != nil {
 		return
 	}
-	fmt.Println(pkg.JsonMarshal(req))
+	if req.Password != req.ConfirmPassword {
+		gtx.String(http.StatusOK, "密码不一致")
+		return
+	}
+	isMatched, err := u.EmailRegexp.MatchString(req.Email)
+	if err != nil {
+		gtx.String(http.StatusOK, fmt.Sprintf("邮箱校验超时 %s", err.Error()))
+		return
+	}
+
+	if !isMatched {
+		gtx.String(http.StatusOK, fmt.Sprintf("邮箱不符合规则 %s", req.Email))
+		return
+	}
 	gtx.String(http.StatusOK, "注册成功")
 }
 
--- webook/backend/main.go
+++ webook/backend/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
+
 	"GolangLearning/webook/backend/internal"
 	"GolangLearning/webook/backend/pkg"
 )
 
 func main() {
 	server := internal.RegisterRoute()
+
+	// 确保 CORS 中间件在最前面
+	server.Use(cors.New(cors.Config{
+		AllowHeaders:     []string{"content-type", "authorization"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return false
+		},
+		MaxAge: 100 * time.Millisecond,
+	}))
+
 	pkg.PanicIf(server.Run(":8080"))
 }
